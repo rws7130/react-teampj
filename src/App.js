196b6c8ff2f2996ffacffd6e586608bdb7f48532
배추 <>
2024-01-07 22:23:23 +0900

    테스트2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React, { useState } from "react";
 import "./App.css";
 
-async function testFetch() {
-  fetch(`${process.env.REACT_APP_SERVER_URL}/goods`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((r) => {
-      return r.json();
-    })
-    .then((obj) => {
-      document.querySelector(".testfetch").innerHTML = JSON.stringify(obj);
-    })
-    .catch((err) => console.log(err));
-}
-
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function testFetch() {
+    setIsLoading(true); // 요청 시작 시 로딩 상태를 true로 설정
+    fetch(`${process.env.REACT_APP_SERVER_URL}/goods`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((r) => r.json())
+      .then((obj) => {
+        document.querySelector(".testfetch").innerHTML = JSON.stringify(obj);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false)); // 요청 완료 시 로딩 상태를 false로 설정
+  }
+
   return (
     <div className="App">
-      <button onClick={testFetch}>연동 테스트2</button>
+      <button onClick={testFetch} disabled={isLoading}>
+        {isLoading ? "연동 중..." : "연동 테스트2"}
+      </button>
       <div className="testfetch"></div>
     </div>
   );
