758f29be0ebbe70502f54bfa92483e6823957976
배추 <>
2024-01-07 21:45:05 +0900

    123123

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  console.log("app.js");
+  console.log(process.env.REACT_APP_SERVER_URL);
+  // fetch(`${process.env.SERVER_URL}/goods`, {
+  //   method: "GET",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // })
+  //   .then((res) => {
+  //     console.log(res);
+  //     return res.json();
+  //   })
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
   return <div className="App">형상관리 푸쉬 테스트1</div>;
 }
 
