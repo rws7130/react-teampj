9e5996641e13d3acf96a7dbd989c778dde3bbe12
YoonDamin <yundamin@naver.com>
2024-01-07 20:17:04 +0900

    push test1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">배포 푸쉬 커밋테스트2</div>;
+  return <div className="App">최종 푸쉬 테스트</div>;
 }
 
 export default App;
