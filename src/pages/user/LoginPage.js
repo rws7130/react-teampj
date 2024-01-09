0ea536e472c903b88c50fc389211d9d3e2fd94a1
고추 <chobuzz23@gmail.com>
2024-01-09 23:49:14 +0900

    fix: adimin - dev6 - BoardList 기능 추가

--- src/pages/user/LoginPage.js
+++ src/pages/user/LoginPage.js
@@ -1,16 +1,17 @@
-import React, {useState} from "react";
-import {useUserStore} from "../../store/useUserStore";
-import {useMutation} from "react-query";
-import {kakaoLogin, login} from "../../api/auth";
+import React, { useState } from "react";
+import { useUserStore } from "../../store/useUserStore";
+import { useMutation } from "react-query";
+import { kakaoLogin, login } from "../../api/auth";
 import {
-  KaKaoLoginButton, LoginBox,
+  KaKaoLoginButton,
+  LoginBox,
   LoginButton,
   LoginCheckbox,
   LoginCheckboxLabel,
   LoginContainer,
-  LoginInput
+  LoginInput,
 } from "../../styles/LoginStyled";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -42,30 +43,30 @@ const LoginPage = () => {
   };
 
   return (
-      <LoginContainer>
-        <LoginBox>
-          <h1>로그인</h1>
-          <LoginInput
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-          />
-          <LoginInput
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-          />
-          <LoginCheckboxLabel htmlFor="memory">아이디 저장</LoginCheckboxLabel>
-          <LoginCheckbox type="checkbox" name="memory" />
-          <LoginButton onClick={handleLogin}>Login</LoginButton>
-          <Link to="/signup">회원가입</Link>
-          <KaKaoLoginButton onClick={handleKaKaoLogin}>
-            카카오 간편로그인
-          </KaKaoLoginButton>
-        </LoginBox>
-      </LoginContainer>
+    <LoginContainer>
+      <LoginBox>
+        <h1>로그인</h1>
+        <LoginInput
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <LoginInput
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <LoginCheckboxLabel htmlFor="memory">아이디 저장</LoginCheckboxLabel>
+        <LoginCheckbox type="checkbox" name="memory" />
+        <LoginButton onClick={handleLogin}>Login</LoginButton>
+        <Link to="/signup">회원가입</Link>
+        <KaKaoLoginButton onClick={handleKaKaoLogin}>
+          카카오 간편로그인
+        </KaKaoLoginButton>
+      </LoginBox>
+    </LoginContainer>
   );
 };
 
--- src/pages/user/dev6/Dev6.js
+++ src/pages/user/dev6/Dev6.js
@@ -1,77 +1,76 @@
 import AdminEditor from "../../../components/AdminEditor";
 import UserDev3 from "../dev3/Dev3";
-import {useEffect} from "react";
+import { useEffect, useState } from "react";
 import createAxiosInstance from "../../../api/axios";
-import {useUserStore} from "../../../store/useUserStore";
-
+import { useUserStore } from "../../../store/useUserStore";
 
 export default function Dev6() {
+  const [boardList, setBoardList] = useState([]);
 
-    const accessToken = useUserStore((state) => state.accessToken);
-    const user = useUserStore((state) => state.user);
-    console.log(user)
-    console.log(accessToken)
-// Axios 인스턴스 생성
-    const axiosInstance = createAxiosInstance('/api6s');
+  const accessToken = useUserStore((state) => state.accessToken);
+  const user = useUserStore((state) => state.user);
+  console.log(user);
+  console.log(accessToken);
+  // Axios 인스턴스 생성
+  const axiosInstance = createAxiosInstance("/api6s");
 
-    const postApi6 = async ()=> {
-        const body = {
-            title : "test123",
-            content : "test123456",
-        };
-        const config = {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            },
-        };
+  const postApi6 = async () => {
+    const body = {
+      title: "test123",
+      content: "test123456",
+    };
+    const config = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
 
-        try {
-            const res = await axiosInstance.post(body, config);
-            if(!res) alert('통신상태 이상')
-            console.log(res);
-
-        } catch (err) {
-            console.error(err);
-
-        }
+    try {
+      const res = await axiosInstance.post(body, config);
+      if (!res) alert("통신상태 이상");
+      console.log(res);
+    } catch (err) {
+      console.error(err);
     }
-    
-    const getApi6 = async () => {
-        try {
-            const res = await axiosInstance.get();
-            console.log(res.data);
-            const boardList = res.data
+  };
 
-        } catch (err) {
-            console.error(err);
-        }
+  const getApi6 = async () => {
+    try {
+      const res = await axiosInstance.get();
+      console.log(res.data);
+      const boardList = res.data;
+      setBoardList(res.data);
+      console.log("BoardList", boardList);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    useEffect(() => {
-
-
-
-    }, []);
+  useEffect(() => {
+    console.log("boardList", boardList);
+  }, [setBoardList]);
 
   return (
     <div>
       <h1>UserDev6</h1>
 
-       <div>
-           <p>Example.com/api6s</p>
-           <p>get, post</p>
-           <p>Example.com/api6s/:api6Id/reply6s</p>
-       </div>
-        <div>
-
-            <button onClick={getApi6}>
-                겟 요청 예시
-            </button>
-            <button onClick={postApi6}>
-                포스트 요청 예시
-            </button>
-        </div>
-
+      <div>
+        <p>Example.com/api6s</p>
+        <p>get, post</p>
+        <p>Example.com/api6s/:api6Id/reply6s</p>
+        {boardList.map((board) => {
+          return (
+            <>
+              <h1>{board.title}</h1>
+              <p>{board.content}</p>
+            </>
+          );
+        })}
+      </div>
+      <div>
+        <button onClick={getApi6}>겟 요청 예시</button>
+        <button onClick={postApi6}>포스트 요청 예시</button>
+      </div>
     </div>
   );
 }
