8ff9fb4ec409874a4a7bf66f9d73911219365f23
당근 <noismik97@gmail.com>
2024-01-09 21:37:48 +0900

    feat: defalutTheme 설정

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { QueryClient, QueryClientProvider } from "react-query";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import styledOptions from './styles/theme';
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <ThemeProvider theme={styledOptions}>
+      <App />
+    </ThemeProvider>
   </QueryClientProvider>
 );
 
---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+const styledOptions = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#1C1C28',
+    G_100: '#28293D',
+    G_200: '#555770',
+    G_300: '#8F90A6',
+    G_400: '#C7C9D9',
+    G_500: '#E4E4EB',
+    G_600: '#EBEBF0',
+    G_700: '#F2F2F5',
+    G_800: '#FAFAFC',
+    red: '#FF3B3B',
+    green: '#06C270',
+    blue: '#0063F7',
+    yellow: '#FFCC00',
+    orange: '#FF8800',
+    teal: '#00CFDE',
+    purple: '#6600CC',
+  },
+  fontSize: {
+    display1: '64px',
+    display2: '56px',
+    h1: '40px',
+    h2: '32px',
+    h3: '28px',
+    h4: '24px',
+    h5: '20px',
+    h6: '16px',
+  },
+};
+
+export default styledOptions;
