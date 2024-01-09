5887259fab6ca312608c7bb68cce36be6f563fa3
배추 <>
2024-01-09 21:52:31 +0900

    유저 라우트 추가

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -13,11 +13,19 @@ const HomePage = () => {
   // }, []);
 
   return (
-    <div>
-      <h1>홈 </h1>
-      <h2>{user?.email}</h2>
-      <Link to="/admin">어드민</Link>
-    </div>
+      <div>
+          <h1>홈 </h1>
+          <h2>{user?.email}</h2>
+          <Link to="/admin">어드민으로 이동</Link>
+          <div>
+              <Link to="/user/dev1">userDev1</Link>
+              <Link to="/user/dev2">userDev2</Link>
+              <Link to="/user/dev3">userDev3</Link>
+              <Link to="/user/dev4">userDev4</Link>
+              <Link to="/user/dev5">userDev5</Link>
+              <Link to="/user/dev6">userDev6</Link>
+          </div>
+      </div>
   );
 };
 
