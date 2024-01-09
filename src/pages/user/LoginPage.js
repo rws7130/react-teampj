93ecbdc396a23930c299f0a857b011b49758f59b
배추 <>
2024-01-09 22:04:10 +0900

    로그인 에러 처리

--- src/pages/user/LoginPage.js
+++ src/pages/user/LoginPage.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { useMutation } from "react-query";
-import { login, kakaoLogin } from "../../../api/auth/auth";
-import { useUserStore } from "../../../store/useUserStore";
-import { Link, useNavigate } from "react-router-dom";
+import React, {useState} from "react";
+import {useUserStore} from "../../store/useUserStore";
+import {useMutation} from "react-query";
+import {kakaoLogin, login} from "../../api/auth";
 import {
-  LoginContainer,
-  LoginBox,
-  LoginInput,
+  KaKaoLoginButton, LoginBox,
   LoginButton,
-  KaKaoLoginButton,
-  LoginCheckboxLabel,
   LoginCheckbox,
+  LoginCheckboxLabel,
+  LoginContainer,
+  LoginInput
 } from "../../styles/LoginStyled";
+import {Link} from "react-router-dom";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -43,30 +42,30 @@ const LoginPage = () => {
   };
 
   return (
-    <LoginContainer>
-      <LoginBox>
-        <h1>로그인</h1>
-        <LoginInput
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <LoginInput
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <LoginCheckboxLabel htmlFor="memory">아이디 저장</LoginCheckboxLabel>
-        <LoginCheckbox type="checkbox" name="memory" />
-        <LoginButton onClick={handleLogin}>Login</LoginButton>
-        <Link to="/signup">회원가입</Link>
-        <KaKaoLoginButton onClick={handleKaKaoLogin}>
-          카카오 간편로그인
-        </KaKaoLoginButton>
-      </LoginBox>
-    </LoginContainer>
+      <LoginContainer>
+        <LoginBox>
+          <h1>로그인</h1>
+          <LoginInput
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+          />
+          <LoginInput
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+          />
+          <LoginCheckboxLabel htmlFor="memory">아이디 저장</LoginCheckboxLabel>
+          <LoginCheckbox type="checkbox" name="memory" />
+          <LoginButton onClick={handleLogin}>Login</LoginButton>
+          <Link to="/signup">회원가입</Link>
+          <KaKaoLoginButton onClick={handleKaKaoLogin}>
+            카카오 간편로그인
+          </KaKaoLoginButton>
+        </LoginBox>
+      </LoginContainer>
   );
 };
 
--- src/pages/user/dev6/Dev6.js
+++ src/pages/user/dev6/Dev6.js
@@ -1,10 +1,35 @@
 import AdminEditor from "../../../components/AdminEditor";
 import UserDev3 from "../dev3/Dev3";
-export default function UserDev6() {
+import {useEffect} from "react";
+export default function Dev6() {
+
+
+    const getApi6 = () => {
+
+    }
+
+    useEffect(() => {
+
+
+
+    }, []);
+
   return (
     <div>
-      <h1>Dev6</h1>
-      <AdminEditor />
+      <h1>UserDev6</h1>
+
+       <div>
+           <p>Example.com/api6s</p>
+           <p>get, post</p>
+           <p>Example.com/api6s/:api6Id/reply6s</p>
+       </div>
+        <div>
+
+            <button onClick={getApi6}>
+                겟요청예시
+            </button>
+        </div>
+
     </div>
   );
 }
