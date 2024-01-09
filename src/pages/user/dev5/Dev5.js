4db104d3d2f086e3c9731c92fc66094287c45569
배추 <>
2024-01-09 21:50:48 +0900

    유저 라우트 추가

---
+++ src/pages/user/dev5/Dev5.js
@@ -0,0 +1,10 @@
+import AdminEditor from "../../../components/AdminEditor";
+import UserDev3 from "../dev3/Dev3";
+export default function UserDev5() {
+  return (
+    <div>
+      <h1>Dev5</h1>
+      <AdminEditor />
+    </div>
+  );
+}
