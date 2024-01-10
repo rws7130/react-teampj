aee32e23bbd380e676a53a6db7e792931f1aec8f
고추 <chobuzz23@gmail.com>
2024-01-10 19:07:50 +0900

    fix: Dev6 axios 작업 중

---
+++ src/api/product.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useUserStore } from "./../store/useUserStore";
+import createAxiosInstance from "./axios";
+
+const MyProduct = ({ endpoint, data }) => {
+  const accessToken = useUserStore((state) => state.accessToken);
+
+  const axiosInstance = createAxiosInstance({ endpoint });
+
+  const postApi = async () => {
+    const body = { data };
+    const config = {
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    try {
+      const res = await axiosInstance.post(body, config);
+      if (!res) alert("통신상태 이상");
+      console.log(res);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getApi = async () => {
+    try {
+      const res = await axiosInstance.get();
+      console.log(res.data);
+      const boardList = res.data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return { getApi, postApi };
+};
+
+export default MyProduct;
--- src/pages/user/dev6/Dev6.js
+++ src/pages/user/dev6/Dev6.js
@@ -3,49 +3,58 @@ import UserDev3 from "../dev3/Dev3";
 import { useEffect, useState } from "react";
 import createAxiosInstance from "../../../api/axios";
 import { useUserStore } from "../../../store/useUserStore";
+import * as MyProduct from "api/product";
 
 export default function Dev6() {
   const [boardList, setBoardList] = useState([]);
 
-  const accessToken = useUserStore((state) => state.accessToken);
-  const user = useUserStore((state) => state.user);
-  console.log(user);
-  console.log(accessToken);
-  // Axios 인스턴스 생성
-  const axiosInstance = createAxiosInstance("/api6s");
+  const data = boardList;
 
-  const postApi6 = async () => {
-    const body = {
-      title: "인풋에잇는 벨류를 타이틀",
-      content: "텍스트 아레아 에 있는 벨류를 컨텐츠로 보냄 예시로, 슬라이드  이미지" ,
-          // image:[''],
-    };
-    const config = {
-      headers: {
-        authorization: `Bearer ${accessToken}`,
-      },
-    };
+  const getMyProduct = MyProduct.getApi({ endpoint: "/api6s" });
 
-    try {
-      const res = await axiosInstance.post(body, config);
-      if (!res) alert("통신상태 이상");
-      console.log(res);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  const postMyProduct = MyProduct.postApi({ endpoint: "/api6s", data });
 
-  const getApi6 = async () => {
-    try {
-      const res = await axiosInstance.get();
-      console.log(res.data);
-      const boardList = res.data;
-      setBoardList(res.data);
-      console.log("BoardList", boardList);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  console.log("getMyProduct", getMyProduct);
+
+  // const accessToken = useUserStore((state) => state.accessToken);
+  // const user = useUserStore((state) => state.user);
+  // console.log(user);
+  // console.log(accessToken);
+  // // Axios 인스턴스 생성
+  // const axiosInstance = createAxiosInstance("/api6s");
+
+  // const postApi6 = async () => {
+  //   const body = {
+  //     title: "인풋에잇는 벨류를 타이틀",
+  //     content: "텍스트 아레아 에 있는 벨류를 컨텐츠로 보냄 예시로, 슬라이드  이미지" ,
+  //         // image:[''],
+  //   };
+  //   const config = {
+  //     headers: {
+  //       authorization: `Bearer ${accessToken}`,
+  //     },
+  //   };
+
+  //   try {
+  //     const res = await axiosInstance.post(body, config);
+  //     if (!res) alert("통신상태 이상");
+  //     console.log(res);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  // const getApi6 = async () => {
+  //   try {
+  //     const res = await axiosInstance.get();
+  //     console.log(res.data);
+  //     const boardList = res.data;
+  //     setBoardList(res.data);
+  //     console.log("BoardList", boardList);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
 
   useEffect(() => {
     console.log("boardList", boardList);
