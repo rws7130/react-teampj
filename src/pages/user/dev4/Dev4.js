5da50f44dcfca5463021d815ae9b93ff46640a71
상추 <diwusutus12@gmail.com>
2024-01-10 21:24:58 +0900

    상추 test 용 추가

--- src/pages/user/dev4/Dev4.js
+++ src/pages/user/dev4/Dev4.js
@@ -3,8 +3,9 @@ import AdminEditor from '../../../components/AdminEditor';
 export default function UserDev4() {
   return (
     <div>
-      <h1>Dev4</h1>
+      <h1>상추</h1>
       <AdminEditor />
+      
     </div>
   );
 }
