6e461aef13ef80a9f5025440d770d9d5f9ea07a0
상추 <diwusutus12@gmail.com>
2024-01-10 23:19:19 +0900

    first setting

--- src/pages/user/dev4/Dev4.js
+++ src/pages/user/dev4/Dev4.js
@@ -3,13 +3,12 @@ import AdminEditor from '../../../components/AdminEditor';
 export default function UserDev4() {
   return (
     <div>
-      <h1>추상</h1>
+      <h1>추상2</h1>
       <AdminEditor />
       
     </div>
   );
 }
-
 // 커밋후에 pull push 
 //기존에 git clone 하면 -> 깃을 초기화했어서 워크스페이스가 2개 
 // 클론할때는 git init을 하면안됨 , 보일러템프릿, 깃인증 
